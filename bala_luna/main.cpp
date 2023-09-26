92aecff85ab036b901cbad7a6637423f8a0eca96
iFrames7 <98622016+iFrames7@users.noreply.github.com>
2023-09-26 12:35:32 -0600

    Ejercicio de fuerzas con bala de cañon en la luna

---
+++ bala_luna/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <box2d/box2d.h>
+
+int main() {
+    b2Vec2 gravity(0.0f, -1.623f);
+
+    b2World world(gravity);
+
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(0.0f, 0.0f);
+
+    b2Body* body = world.CreateBody(&bodyDef);
+    
+
+    b2CircleShape dynamicBall;
+    dynamicBall.m_p.Set(bodyDef.position.x, bodyDef.position.y);
+    dynamicBall.m_radius =0.0145f;
+
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &dynamicBall;
+    fixtureDef.density = 7850.0f;
+    fixtureDef.friction = 0.3f;
+
+    body->CreateFixture(&fixtureDef);
+
+    //Setear variables para lanzamiento
+    float angle = 60.0f;
+    float speed = 250.0f;
+
+    //Componentes de fuerza descompuesto en x y y
+    float vx = speed * std::cos(angle*3.14/180);
+    float vy = speed * std::sin(angle*3.14/180);
+
+    body->SetLinearVelocity(b2Vec2(vx, vy));
+
+    float timeStep = 1.0f/60.0f;
+
+    int32 velocityIterations = 6;
+    int32 positionIterations = 2;
+
+    for (int32 i=0; i<60; i++){
+        world.Step(timeStep, velocityIterations, positionIterations);
+        b2Vec2 position = body->GetPosition();
+        printf("%4.2f %4.2f\n", position.x, position.y);
+    }
+}
